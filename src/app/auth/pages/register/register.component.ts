c29ed94b407f9c7bac02c20a90ca3f4d95958a64
Adrian Fernandez <adrianxtuu@gmail.com>
2023-03-24 22:46:05 -0400

    implementacion de formulario reactivos

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  
+
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    name: ['Adrian', [Validators.required]],
+    email: ['test1@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
 
-  ngOnInit(): void {
+  })
+
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+
+  registro() {
+console.log(this.miFormulario.value)
+console.log(this.miFormulario.valid)
+
   }
 
 }
